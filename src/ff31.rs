9542133cfe8cdf1baa5ce310a45908983be00d8e
Rani Sharim <rani.sharim@gmail.com>
2022-04-02 19:47:58 +0300

    Basic enc works

--- src/ff31.rs
+++ src/ff31.rs
@@ -1,8 +1,8 @@
 use aes::cipher::{generic_array::GenericArray, BlockEncrypt, KeyInit};
 use aes::Aes128;
 
-use num_bigint::{BigUint, ToBigUint};
-use std::ops::{Add, Sub};
+use num_bigint::{BigInt, Sign, ToBigInt};
+use std::ops::{Add, Div, Mul, Sub};
 pub struct FF31<'a> {
     key: &'a [u8],
     radix: u32,
@@ -24,8 +24,8 @@ impl<'a> FF31<'a> {
 
     fn enc_from_string(&self, plain_text: &'a str) -> Vec<u32> {
         let mut res = Vec::with_capacity(plain_text.len());
-        plain_text.chars().for_each(|c|{
-            for (i,ac) in self.alphabet.char_indices() {
+        plain_text.chars().for_each(|c| {
+            for (i, ac) in self.alphabet.char_indices() {
                 if c == ac {
                     res.push(i as u32);
                     break;
@@ -34,10 +34,10 @@ impl<'a> FF31<'a> {
         });
         res
     }
-    
+
     fn enc_to_string(&self, decrypted_str: &[u32]) -> String {
         let mut res = String::with_capacity(decrypted_str.len());
-        decrypted_str.into_iter().for_each(|d| {
+        decrypted_str.iter().for_each(|d| {
             res.push(self.alphabet.chars().nth(*d as usize).unwrap());
         });
         res
@@ -83,7 +83,7 @@ impl<'a> FF31<'a> {
                 w = t_l;
             }
 
-            p[..3].copy_from_slice(&w[..3]);
+            p[..4].copy_from_slice(&w[..4]);
 
             if is_enc {
                 p[3] ^= i as u8;
@@ -91,20 +91,25 @@ impl<'a> FF31<'a> {
                 p[3] ^= (7 - i) as u8;
             }
 
-            let nb = to_bytes(revs(&b));
+            let c = to_bigint(&revs(&b), self.radix);
 
-            if nb.len() >= 12 {
+            let nb = c.to_bytes_be().1;
+
+            let nb_len = nb.len();
+            if nb_len >= 12 {
                 p[4..].copy_from_slice(&nb[..12])
             } else {
-                p[4..].copy_from_slice(&nb[..])
+                let p_len = p.len();
+                p[4..p_len - nb_len].fill(0);
+                p[p_len - nb_len..].copy_from_slice(&nb[..])
             }
 
             p.reverse();
-            ciph(&mut p, self.key);
+            self.ciph(&mut p);
             p.reverse();
 
-            let y = BigUint::from_bytes_be(&p);
-            let mut c = BigUint::from_bytes_be(&to_bytes(revs(&a)));
+            let y = BigInt::from_bytes_be(Sign::Plus, &p);
+            let mut c = to_bigint(&revs(&a), self.radix);
 
             if is_enc {
                 c = c.add(y);
@@ -112,13 +117,13 @@ impl<'a> FF31<'a> {
                 c = c.sub(y);
             }
 
-            let r = self.radix.to_biguint().unwrap();
-            r.pow(m);
-            c = c.modpow(&(1i32.to_biguint().unwrap()), &r);
+            let mut r = self.radix.to_bigint().unwrap();
+            r = r.pow(m);
+
+            c = c.modpow(&(1i32.to_bigint().unwrap()), &r);
+
             a = b;
-            let s = to_str(&c);
-            let revs = revs(&s);
-            b = revs.to_vec();
+            b = to_rev_str(c, self.radix, u);
         }
 
         if is_enc {
@@ -129,14 +134,14 @@ impl<'a> FF31<'a> {
             b
         }
     }
-}
 
-fn ciph(x: &mut [u8; 16], k: &[u8]) {
-    let key = GenericArray::from_slice(k);
-    let block = GenericArray::from_mut_slice(x);
+    fn ciph(&self, x: &mut [u8; 16]) {
+        let key = GenericArray::from_slice(self.key);
+        let block = GenericArray::from_mut_slice(x);
 
-    let cipher = Aes128::new(key);
-    cipher.encrypt_block(block);
+        let cipher = Aes128::new(key);
+        cipher.encrypt_block(block);
+    }
 }
 
 fn revs(x: &[u32]) -> Vec<u32> {
@@ -145,25 +150,39 @@ fn revs(x: &[u32]) -> Vec<u32> {
     y
 }
 
-fn to_bytes(x: Vec<u32>) -> Vec<u8> {
-    let mut res = Vec::with_capacity(x.len() * 4);
-    x.into_iter().for_each(|u| {
-        res.extend_from_slice(&u.to_be_bytes());
-    });
+fn to_rev_str(mut x: BigInt, radix: u32, m: usize) -> Vec<u32> {
+    let mut res = Vec::with_capacity(m);
+
+    let e = 1u32.to_bigint().unwrap();
+    let r = radix.to_bigint().unwrap();
+    let z = 0u32.to_bigint().unwrap();
+
+    while x > z {
+        let d = x.modpow(&e, &r);
+
+        if d == z {
+            res.push(0);
+        } else {
+            res.push(d.to_u32_digits().1[0]);
+        }
+
+        x = x.div(radix);
+    }
+
+    while res.len() < m {
+        res.push(0);
+    }
+
     res
 }
 
-fn from_bytes(x: &[u8]) -> Vec<u32> {
-    let mut res = Vec::new();
-    x.chunks_exact(4).for_each(|b| {
-        let mut be: [u8; 4] = [0u8; 4];
-        be.copy_from_slice(b);
-        res.push(u32::from_be_bytes(be));
-    });
+fn to_bigint(n: &[u32], radix: u32) -> BigInt {
+    let mut b = BigInt::from_bytes_be(Sign::NoSign, &[0u8]);
 
-    res
-}
+    for d in n.iter() {
+        b = b.mul(radix);
+        b = b.add(d);
+    }
 
-fn to_str(x: &BigUint) -> Vec<u32> {
-    from_bytes(&x.to_bytes_be())
+    b
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 pub mod ff31;
 
 fn main() {
-    let x = ff31::FF31::new(&[1, 2, 3], "abc");
+    // abcdefghijklmnopqrstuvwxyz
+    let x = ff31::FF31::new(&[0u8; 16], "0123456789");
     let tweak = [0; 7];
 
-    let enc = x.encrypt("abc", &tweak);
-    let dec = x.decrypt(&enc, &tweak);
+    let enc = x.encrypt("001001", &tweak);
+    // let dec = x.decrypt(&enc, &tweak);
 
-    println!("{:?}", &dec);
+    // println!("{:?}", &dec);
 }
